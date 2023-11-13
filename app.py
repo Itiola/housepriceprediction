ef56b78c3a4b3b23cfd7b28688ce7a9f84a3da9f
Babatunde Fortune Itiola <itiola579@gmail.com>
2023-11-13 10:48:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ st.title('Smart Home Prediction')
 st.image("home.jpg", use_column_width=True)
 
 st.number_input("Enter the area", 0)
-    
+st.number_input("Number of bedrooms", 0)
+st.number_input("Number of bathrooms", 0)
+st.number_input("Number of stories", 0)
+st.number_input("Number of parking spaces", 0)
+
