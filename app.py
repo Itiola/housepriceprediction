b6166b5bf6fb46ab0f21e2020318cee556277cca
Babatunde Fortune Itiola <itiola579@gmail.com>
2023-11-13 11:46:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ st.selectbox("Has prefarea", ["Yes","No"])
 st.radio("Furnishing status", ["Unfurnished", "Semi-furnished", "Furnished"])
 st.button("Predict", type="primary")
 if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
+#    st.write('W')
+#else:
+ #   st.write('Goodbye')
 
 
 
